df7dadf23a60184cd221fe45c91a4f1a85759178
SabahTARTIR365 <sosotarteer2017@gmail.com>
2022-03-29 10:00:08 +0300

    initialization commit

---
+++ CalculatorTask/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace CalculatorTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("----*************************----");
+          
+
+        }
+
+
+        
+
+    
+
+    
+    }
+}
+
+
+
