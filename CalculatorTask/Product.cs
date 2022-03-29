5beaab4c814907de25d8a752cd6b38c839fb2e69
SabahTARTIR365 <sosotarteer2017@gmail.com>
2022-03-29 10:33:00 +0300

    create product class

---
+++ CalculatorTask/Product.cs
@@ -0,0 +1,20 @@
+﻿namespace CalculatorTask
+{
+    internal class Product
+    {
+        private string productName;
+        private int universalProductCode;
+        private double dproductPrice;
+
+        public Product()
+        {
+        }
+
+        public Product(string v1, int v2,  double v3)
+        {
+            this.productName = v1;
+            this.universalProductCode = v2;
+            this.dproductPrice = v3;
+        }
+    }
+}
