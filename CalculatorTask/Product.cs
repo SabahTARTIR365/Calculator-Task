8412ed1359390a0fe884f455557f2ec686d71658
SabahTARTIR365 <sosotarteer2017@gmail.com>
2022-03-29 16:34:43 +0300

    create function for discounte but not completed yet

--- CalculatorTask/Product.cs
+++ CalculatorTask/Product.cs
@@ -5,25 +5,45 @@
         private string productName;
         private int universalProductCode;
         private double productPrice;
-        private double productTax=0.2;
+        private double productTaxPercentage=0.2;
+        private double productDiscountPercentage = 0.15;
+        private double productDiscountAmount;
+        private double productTaxAmount;
         public Product()
         {
         }
 
-
-        public Product(string v1, int v2,  double v3)
+        public Product(string productName, int universalProductCode,  double productPrice)
         {
-            this.productName = v1;
-            this.universalProductCode = v2;
-            this.productPrice = v3;
+            this.productName = productName;
+            this.universalProductCode = universalProductCode;
+            this.productPrice = productPrice;
+        }
+
+        internal void addPriceDiscount()
+        {
+            if (this != null)
+            {
+                Console.WriteLine($"Product price reported as ${Math.Round(this.productPrice, 2)} before discount");
+                this.productDiscountAmount =  (this.productPrice * productDiscountPercentage);
+                this.productPrice = this.productPrice - this.productDiscountAmount;
+                Console.WriteLine($"and ${ Math.Round(this.productPrice, 2)} after { this.productDiscountPercentage * 100}% discount.");
+            }
+        }
+
+        internal void getFinalPrice()
+        {
+            this.addPriceTax();
+            this.addPriceDiscount();
         }
 
         internal void addPriceTax()
         {
            if (this  != null) {
                 Console.WriteLine($"Product price reported as ${Math.Round(this.productPrice, 2)} before tax");
-               this.productPrice = this.productPrice+ (this.productPrice * 0.2);
-                Console.WriteLine($"and ${ Math.Round(this.productPrice, 2)} after { productTax*100}% tax.");
+               this.productTaxAmount = this.productPrice * productTaxPercentage;
+                this.productPrice = this.productPrice + this.productTaxAmount;
+                Console.WriteLine($"and ${ Math.Round(this.productPrice, 2)} after { productTaxPercentage*100}% tax.");
         }
         }
     }
--- CalculatorTask/Program.cs
+++ CalculatorTask/Program.cs
@@ -4,12 +4,11 @@ namespace CalculatorTask
     class Program
     {
         static void Main(string[] args)
-        {
-            // Console.WriteLine("Hello World!");
-            Product product = new Product("The Little Prince", 12345,20.25666);
-            product.addPriceTax();
-            //Tax tsx = new Tax(20.25);
-           // tsk.add
+        { 
+            Product product = new Product("The Little Prince", 12345,20.25);
+           // product.addPriceTax();
+            product.getFinalPrice();
+          
 
         }
 
