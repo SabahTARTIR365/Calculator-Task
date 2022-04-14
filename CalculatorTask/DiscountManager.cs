ab403f0eea25024bc4825a7f05ac422f80561f59
SabahTARTIR365 <sosotarteer2017@gmail.com>
2022-04-14 14:49:08 +0300

    add discount manager class for #7 requierment

---
+++ CalculatorTask/DiscountManager.cs
@@ -0,0 +1,18 @@
+﻿
+namespace CalculatorTask
+{
+    internal class DiscountManager
+    {
+        private List<Discount> discounts;
+        private bool isMultiplicativeDiscount;
+
+        public DiscountManager(List<Discount> discounts, bool isMultiplicativeDiscount)
+        {
+            this.Discounts = discounts;
+            this.IsMultiplicativeDiscount = isMultiplicativeDiscount;
+        }
+
+        public bool IsMultiplicativeDiscount { get => isMultiplicativeDiscount; set => isMultiplicativeDiscount = value; }
+        internal List<Discount> Discounts { get => discounts; set => discounts = value; }
+    }
+}
