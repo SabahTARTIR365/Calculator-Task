bf44b2ac2d0dc4044fa89dc1167e480e9660d0c0
SabahTARTIR365 <sosotarteer2017@gmail.com>
2022-04-24 14:57:57 +0300

    create cap class

---
+++ CalculatorTask/Cap.cs
@@ -0,0 +1,17 @@
+﻿namespace CalculatorTask
+{
+    internal class Cap
+    {
+        private double value;
+        private bool isCapPersentage;
+
+        public Cap(double value, bool isPercentage)
+        {
+            this.Value = value;
+            this.IsCapPersentage = isPercentage;
+        }
+
+        public double Value { get => value; set => this.value = value; }
+        public bool IsCapPersentage { get => isCapPersentage; set => isCapPersentage = value; }
+    }
+}
