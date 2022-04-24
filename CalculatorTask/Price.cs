36265e6ace2a08e3ec5adc9c6e9563deee290911
SabahTARTIR365 <sosotarteer2017@gmail.com>
2022-04-24 22:58:32 +0300

    create Price class and use it as parameter for product class and refator the old double value

---
+++ CalculatorTask/Price.cs
@@ -0,0 +1,14 @@
+﻿namespace CalculatorTask
+{
+    internal class Price
+    {
+        private double v;
+        private object uSD;
+
+        public Price(double v, object uSD)
+        {
+            this.v = v;
+            this.uSD = uSD;
+        }
+    }
+}
--- CalculatorTask/Program.cs
+++ CalculatorTask/Program.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using Bogus.DataSets;
+using System;
+using System.Globalization;
 namespace CalculatorTask
 {
     class Program
     {
         static void Main(string[] args)
-        { 
-            Product product = new Product("The Little Prince", 12345, 20.25);
+        {
+      
+           Price price = new Price(20.25, "USD");
+
+            Product product = new Product("The Little Prince", 12345, price);
             Tax tax = new Tax(0.21);
             List<Discount> Discounts = new List<Discount>();
             Discount Upc = new Discount(0.07,false,"UPC");
@@ -14,7 +19,7 @@ namespace CalculatorTask
             Discounts.Add(Upc);
             //Discounts.Add(Upc1);
             Discounts.Add(UniDiscount);
-            Cost packaging = new Cost(0.01, true,"Packaging cost");
+            Cost packaging = new Cost(0.01, true, "Packaging cost");
             Cost transport = new Cost(2.2,false, "Transport cost");
             List <Cost> costs = new List<Cost>();
             Cap discontCap = new Cap(0.20,true);
